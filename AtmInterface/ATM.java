04c05b71469e4d2a7902c70fe5d47f6d1cb85a79
CHILAKA THANUJA <123240724+chthanuja@users.noreply.github.com>
2023-08-15 00:41:21 +0530

    Create ATM.java

---
+++ AtmInterface/ATM.java
@@ -0,0 +1,25 @@
+public class ATM {
+    private float balance;
+    private float depositAmt;
+    private float withdrawAmt;
+    public ATM(){
+    }
+public float getBalance() {
+        return balance;
+    }
+    public void setBalance(float balance) {
+        this.balance = balance;
+    }
+    public float getDepositAmount() {
+        return depositAmt;
+    }
+    public void setDepositAmount(float depositAmount) {
+        depositAmount = depositAmt;
+    }
+    public float getWithdrawAmount() {
+        return withdrawAmt;
+    }
+    public void setWithdrawAmount(float withdrawAmount) {
+        withdrawAmount = withdrawAmt;
+    }
+}
